80599dfd3fd7832377ac7bcaf81b0035ae12fa0f
prakao <kpkrisrin@gmail.com>
2026-01-08 22:17:56 +0700

    13_1_finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,39 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double nums[],int range,double result[] ){
+    double sum =0;
+    for(int i=0;i<range;i++) sum+=nums[i];
+    
+    double mean=sum/range;
+    result[0]=mean;
+    
+    double a=0;
+    for(int i=0;i<range;i++) a+=(nums[i]-mean)*(nums[i]-mean);
+    
+    double sd = sqrt(a/range);
+    result[1]=sd;
+
+    double b=1;
+    for(int i=0;i<range;i++) b*=nums[i];
+
+    double gm = pow(b,(1.0/range));
+    result[2]=gm;
+
+    double c=0;
+    for(int i=0;i<range;i++) c+=(1.0/nums[i]);
+
+    double hm = range/c;
+    result[3]=hm;
+
+    double min=nums[0],max=nums[0];
+    
+    for(int i=0;i<range;i++){ 
+    if(nums[i]>max)max=nums[i];
+    if(nums[i]<min)min=nums[i];
+    }
+    result[4]=max;
+    result[5]=min;
+
+}
